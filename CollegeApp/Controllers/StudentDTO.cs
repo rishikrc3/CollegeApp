b23fcaec489b1adf43e94a4d7155518a3b1f2caa
rishik <rishikrc3@gmail.com>
2024-07-12 01:39:30 +0530

    DTO Class

---
+++ CollegeApp/Controllers/StudentDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CollegeApp.Controllers
+{
+	public class StudentDTO
+	{
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+    }
+}
+
