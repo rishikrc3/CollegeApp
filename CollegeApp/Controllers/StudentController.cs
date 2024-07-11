02d31336ac2e9a239fd6dfc5bd0ebe5f5431cf50
rishik <rishikrc3@gmail.com>
2024-07-11 20:23:06 +0530

    Basic_api_testing_

---
+++ CollegeApp/Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeApp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class StudentController:ControllerBase
+	{
+		[HttpGet]
+		[Route("/name")]
+		public string StudentName()
+		{
+			return "Rishik";
+		}
+	}
+}
+
