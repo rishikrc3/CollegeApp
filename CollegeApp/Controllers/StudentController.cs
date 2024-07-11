b7305127bd10adb1747d0e235ee02a52f4c14539
rishik <rishikrc3@gmail.com>
2024-07-12 00:07:40 +0530

    DeleteById

--- CollegeApp/Controllers/StudentController.cs
+++ CollegeApp/Controllers/StudentController.cs
@@ -9,26 +9,33 @@ namespace CollegeApp.Controllers
     public class StudentController : ControllerBase
     {
         [HttpGet]
-        [Route("name")]
+        [Route("All" ,Name ="GetAllStudents")]
         public IEnumerable<Student> StudentName()
         {
             return CollegeRepository.Students;
         }
 
         [HttpGet]
-        [Route("{id:int}")]
+        [Route("{id:int}", Name ="GetStudentsById")]
         public Student GetStudentById(int id)
         {
             return CollegeRepository.Students.Where(n => n.Id == id).FirstOrDefault();
         }
 
         [HttpGet]
-        [Route("{name}")]
+        [Route("{name}", Name ="GetStudentByName")]
         public Student GetStudentByName(string name)
         {
             return CollegeRepository.Students.Where(n => n.StudentName == name).FirstOrDefault();
         }
-       
+        [HttpDelete]
+        [Route("{id:int}" , Name ="DeleteStudentById")]
+        public bool DeleteStudent(int id)
+        {
+            var studentToBeDeleted = CollegeRepository.Students.Where(n => n.Id == id).FirstOrDefault();
+            CollegeRepository.Students.Remove(studentToBeDeleted);
+            return true;
+        }
     }
 }
 
