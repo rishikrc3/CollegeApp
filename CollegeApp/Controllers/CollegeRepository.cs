05ed6813c1fe5cd71bff9f8ea99d5c01dd87540a
rishik <rishikrc3@gmail.com>
2024-07-11 23:43:15 +0530

    addign Repository Class

---
+++ CollegeApp/Controllers/CollegeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CollegeApp.Controllers
+{
+	public class CollegeRepository
+	{
+        public static List<Student> Students { get; set; } = new List<Student>() {
+             new Student
+                {
+                    Id = 1,
+                    StudentName = "RishikC",
+                    Email = "rishikr3@gmail.com",
+                    Address = "Kalyanpur"
+                },
+             new Student
+                {
+                    Id = 2,
+                    StudentName = "Disha",
+                    Email = "bidisha2125@gmail.com",
+                    Address = "Garia"
+                }
+        };
+		
+	}
+}
+
--- CollegeApp/Controllers/StudentController.cs
+++ CollegeApp/Controllers/StudentController.cs
@@ -1,34 +1,20 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace CollegeApp.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class StudentController:ControllerBase
-	{
-		[HttpGet]
-		[Route("/name")]
-		public IEnumerable<Student> StudentName()
-		{
-            return new List<Student>
-            {
-                new Student
-                {
-                    Id = 1,
-                    StudentName = "RishikC",
-                    Email = "rishikr3@gmail.com",
-                    Address = "Kalyanpur"
-                },
-                new Student
-                {
-                    Id = 2,
-                    StudentName = "Disha",
-                    Email = "bidisha2125@gmail.com",
-                    Address = "Garia"
-                }
-            };
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/name")]
+        public IEnumerable<Student> StudentName()
+        {
+            return CollegeRepository.Students;
         }
-	}
+       
+    }
 }
 
